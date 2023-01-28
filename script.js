63b62261f8a78b703edfc3afd0d32c13ab7c2346
Victor França <119083249+FrancVictor@users.noreply.github.com>
2023-01-27 22:40:36 -0300

    almoxarifs

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+
+function add() {
+  
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("O dia já foi adicionado🔴")
+    return
+  }
+
+  alert("Dia adicionado com sucesso!✅")
+  nlwSetup.addDay(today)
+}
+
+function save(){
+  localStorage.setItem("habitsLS", JSON.stringify(nlwSetup.data))
+}
+
+
+
+const data = JSON.parse(localStorage.getItem("habitsLS")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
